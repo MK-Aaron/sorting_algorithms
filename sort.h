5c0ad848f58b62a6e3a2d21e4b4a9544eba5f3d8
MK-Aaron <mwendwa_aaron78@yahoo.com>
2024-01-23 15:45:45 -0800

    Selection sort

--- sort.h
+++ sort.h
@@ -1,6 +1,7 @@
 #ifndef SORT_H
 #define SORT_H
 
+#include <stdio.h>
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -18,6 +19,7 @@ typedef struct listint_s
 /* Prototypes for mandatory tasks */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
 
 /* Prototypes for print functions */
 void print_list(const listint_t *list);
